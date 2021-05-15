c6769610440fe0120fa71f113ac74774cda21012
YurenSUN <ysun299@wisc.edu>
2021-05-15 13:17:56 -0500

    add some projects

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Computer-Graphics-Projects
 This projects created for the Computer Graphics class at the University of Wisconsin - Madison in Spring 2021 using the class software framework
+
+The files in libs folder, all html files, and some codes in the js files were provided by the instructors (js framworks) or from the online sources (attributed in the js files).
---
+++ flipDots.js
@@ -0,0 +1,96 @@
+// we do enable typescript type checking - see
+// https://graphics.cs.wisc.edu/Courses/559-sp2021/pages/typed-js/
+// and
+// https://github.com/Microsoft/TypeScript/wiki/Type-Checking-JavaScript-Files
+// @ts-check
+
+/* Set options for jshint (my preferred linter)
+ * disable the warning about using bracket rather than dot
+ * even though dot is better
+ * https://stackoverflow.com/questions/13192466/how-to-suppress-variable-is-better-written-in-dot-notation
+ */
+/* jshint -W069, esversion:6 */
+
+export { };
+
+// circles created by users
+let dots = [];
+let radius = 10;
+let colors = ["green", "yellow"]; // color to change between
+let colors_clicked = ["red", "orange"]; // color to change between
+
+
+/** @type {HTMLCanvasElement} */
+let canvas = (/** @type {HTMLCanvasElement} */ document.getElementById("box1canvas"));
+let context = canvas.getContext('2d');
+
+// when user click, add the circle
+canvas.onclick = function (event) {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    let box = /** @type {HTMLCanvasElement} */(event.target).getBoundingClientRect();
+    const mx = x - box.left;
+    const my = y - box.top;
+
+    // first check whether click on the existing circle
+    let exist = false;
+    dots.forEach(function (dot) {
+        const dis_x = mx - dot.x;
+        const dis_y = my - dot.y;
+        if (Math.sqrt(dis_x * dis_x + dis_y * dis_y) <= radius) {
+            // existing circle, change color
+            dot.clicked = !dot.clicked;
+            dot.mouseOn = false;
+            exist = true;
+        }
+    });
+    if (!exist){
+        dots.push({ "x": mx, "y": my, "r": radius, "mouseOn": false,  "clicked":false });
+    }
+};
+
+// change the color of circle if the mouse is on the circle
+canvas.onmousemove = function (event) {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    let box = /** @type {HTMLCanvasElement} */(event.target).getBoundingClientRect();
+    const mx = x - box.left;
+    const my = y - box.top;
+
+    // check the distance between mouse and center of the circle
+    dots.forEach(function (dot) {
+        const dis_x = mx - dot.x;
+        const dis_y = my - dot.y;
+
+        if (Math.sqrt(dis_x * dis_x + dis_y * dis_y) <= radius) {
+            // on the circle, change color
+            dot.mouseOn = true;
+        } else {
+            dot.mouseOn = false;
+        }
+    });
+
+};
+
+function boxanimate() {
+    // clear the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // draw all of the dots
+    dots.forEach(function (dot) {
+        if (dot.clicked){
+            context.fillStyle = dot.mouseOn ? colors_clicked[1] : colors_clicked[0];
+        }else{
+            context.fillStyle = dot.mouseOn ? colors[1] : colors[0];
+        }
+        context.beginPath();
+        context.arc(dot.x, dot.y, dot.r, 0, Math.PI * 2, false);
+        context.closePath();
+        context.fill();
+    });
+    window.requestAnimationFrame(boxanimate);
+}
+boxanimate();
+
